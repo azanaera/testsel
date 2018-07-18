9dac6e50998d7a01cdcf8b930f120bbde93350ea
Era <azanaera691@gmail.com>
2018-07-18 20:08:22 +0800

    added explicit wait

--- src/test/java/testcases/AdminTabTest.java
+++ src/test/java/testcases/AdminTabTest.java
@@ -9,6 +9,10 @@ import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 import gurupages.DemoGuru;
@@ -30,6 +34,10 @@ public class AdminTabTest extends SampleTest {
 	public void navToAdmin(String user, String pass) throws Exception {
 		super.loginPC(user, pass);
 		super.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebElement element = wait.until(
+		ExpectedConditions.visibilityOfElementLocated(By.id("TabBar:AdminTab-btnInnerEl")));
 		super.polCenter.navigateToAdminTab();
 	}
 }
